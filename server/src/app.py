7346c2ead128ca12ce5d0da4eb797026d48af600
Mohamed Safwan <mohamedsafwan220@gmail.com>
2025-01-25 16:16:46 +0530

    upgraded server

--- server/src/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from movie import movie_blueprint
-
-app = Flask(__name__)
-
-app.register_blueprint(movie_blueprint)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
